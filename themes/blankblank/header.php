9ce6001acc49935e128d60ac9f78ac02bdd69a4c
SamTyurenkov <sam.tyurenkov@gmail.com>
2021-04-03 00:23:32 +0300

    yoast

--- themes/blankblank/header.php
+++ themes/blankblank/header.php
@@ -1,3 +1,8 @@
+<?php
+$yoast_title = get_post_meta($post_id, '_yoast_wpseo_title', true);
+$yoast_meta = get_post_meta($post->ID, '_yoast_wpseo_metadesc', true);
+?>
+
 <!doctype html>
 
   <html <?php language_attributes(); ?> >
@@ -11,6 +16,11 @@
 		<!-- Mobile Meta -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+		<?php if(is_home() || is_frontpage()) : ?>
+		<title><?php if ($yoast_meta) echo esc_html($yoast_title); ?></title>
+		<meta name="description" content="<?php if ($yoast_meta) echo esc_attr($yoast_meta); ?>" />
+		<?php endif; ?>
 
 		<?php wp_head(); ?>
 
