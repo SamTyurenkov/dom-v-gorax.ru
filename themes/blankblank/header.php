f21ba926bf87c6c608ca604ceaeb4f585c59aa33
SamTyurenkov <sam.tyurenkov@gmail.com>
2021-04-03 00:24:37 +0300

    fix

--- themes/blankblank/header.php
+++ themes/blankblank/header.php
@@ -17,8 +17,8 @@ $yoast_meta = get_post_meta($post->ID, '_yoast_wpseo_metadesc', true);
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-		<?php if(is_home() || is_frontpage()) : ?>
-		<title><?php if ($yoast_meta) echo esc_html($yoast_title); ?></title>
+		<?php if(is_home() || is_front_page()) : ?>
+		<title><?php if ($yoast_title) echo esc_html($yoast_title); ?></title>
 		<meta name="description" content="<?php if ($yoast_meta) echo esc_attr($yoast_meta); ?>" />
 		<?php endif; ?>
 
